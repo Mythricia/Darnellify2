b3f8d7c1ec486f62c1338d88d8292126e2762372
Mythricia <mythricia@gmail.com>
2019-06-12 02:16:29 +0200

    Improved release script to append git tag to zip filename

--- PackageForRelease.bat
+++ PackageForRelease.bat
@@ -1,9 +1,14 @@
 @echo off
 
 REM REQUIRES 7zip to be installed
+REM WILL DELETE any Darnellify2 .zip files in the directiory!
 REM This batch file packages the addon into a releaseable .zip
 REM Sounds are NOT INCLUDED ON GIT, make sure to get them first
 
-del Darnellify2.zip
+REM Get the active Git tag
+for /f %%i in ('git tag') do set TAG=%%i
 
-7z a -tzip Darnellify2.zip -r *.lua Readme.txt Darnellify2.toc Sounds\
+REM Remove any existing Darnellify2 version zips
+del /Q Darnellify2*.zip
+
+7z a -tzip Darnellify2_%TAG%.zip -r *.lua Readme.txt Darnellify2.toc Sounds\
