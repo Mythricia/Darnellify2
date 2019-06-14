eb7e5dbb064a4ef2b3209c02b04a41177c4841fd
Mythricia <mythricia@gmail.com>
2019-06-14 06:59:41 +0200

    Updated packaging script
    * Now puts new zips into a /Builds directory
    * No longer wipes out all old zips, only exact name-duplicates

--- PackageForRelease.bat
+++ PackageForRelease.bat
@@ -5,10 +5,12 @@ REM WILL DELETE any Darnellify2 .zip files in the directiory!
 REM This batch file packages the addon into a releaseable .zip
 REM Sounds are NOT INCLUDED ON GIT, make sure to get them first
 
+mkdir Builds
+
 REM Get the active Git tag
 for /f %%i in ('git tag --sort=v:refname') do set TAG=%%i
 
-REM Remove any existing Darnellify2 version zips
-del /Q Darnellify2*.zip
+REM Remove any existing zip of this version
+del /Q Builds\Darnellify2_%TAG%.zip
 
-7z a -tzip Darnellify2_%TAG%.zip -r *.lua Readme.txt Darnellify2.toc Sounds\
+7z a -tzip Builds\Darnellify2_%TAG%.zip -r *.lua Readme.txt Darnellify2.toc Sounds\
