7a5e369b3807fa7c8e02034f3b64c900513972f1
Mythricia <mythricia@gmail.com>
2019-06-14 06:41:13 +0200

    Updated to sort tags by version and use the latest one

--- PackageForRelease.bat
+++ PackageForRelease.bat
@@ -6,7 +6,7 @@ REM This batch file packages the addon into a releaseable .zip
 REM Sounds are NOT INCLUDED ON GIT, make sure to get them first
 
 REM Get the active Git tag
-for /f %%i in ('git tag') do set TAG=%%i
+for /f %%i in ('git tag --sort=v:refname') do set TAG=%%i
 
 REM Remove any existing Darnellify2 version zips
 del /Q Darnellify2*.zip
